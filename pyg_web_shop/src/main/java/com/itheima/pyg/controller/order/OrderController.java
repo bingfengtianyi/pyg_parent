2fe7ef60d14d749e8c79621e9f03ceade45ab7ab
lsp <lsp@136.com>
2018-11-29 16:44:25 +0800

    张雨晨-商家后台分页查询订单

--- pyg_web_shop/src/main/java/com/itheima/pyg/controller/order/OrderController.java
+++ pyg_web_shop/src/main/java/com/itheima/pyg/controller/order/OrderController.java
@@ -7,12 +7,14 @@ import com.itheima.pyg.entity.ZImageResult;
 import com.itheima.pyg.pojo.order.Order;
 import com.itheima.pyg.service.order.OrderService;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("order")
@@ -26,5 +28,30 @@ public class OrderController {
         String seller = SecurityContextHolder.getContext().getAuthentication().getName();
         return orderService.getOrderListByPageAndSeller(pageNum,pageSize,seller);
     }
+
+
+
+
+    @RequestMapping("/search")
+    public PageResult selectOrder(Integer page, Integer rows, @RequestBody Order order) {
+        String sellerName = SecurityContextHolder.getContext().getAuthentication().getName();//获取当前用户登录信息
+        order.setSellerId(sellerName);
+        System.out.println(sellerName);
+        PageResult pageResult = orderService.selectOrder(page, rows, order);
+        return pageResult;
+    }
+
+
+
+    /**
+     * 商家后台订单查询
+     *
+     * @return
+     */
+    @RequestMapping("/findAll")
+    public List<Order> findAll() {
+        return orderService.findAll();
+    }
+
 
 }
