b80ad156727b5b0929cebdad47e51e765aad6075
jiangbo <jb112224@sina.com>
2018-11-30 10:37:55 +0800

    姜博 修改地址管理

--- pyg_interface/src/main/java/com/itheima/pyg/service/user/AddressService.java
+++ pyg_interface/src/main/java/com/itheima/pyg/service/user/AddressService.java
@@ -6,4 +6,20 @@ import java.util.List;
 
 public interface AddressService {
     List<Address>   findListByLoginUser(String userId);
+
+    //查询用户收货地址
+    public List<Address>findListByUserName(String userName);
+
+    //新增用户收货地址
+    public void addAddress(Address address, String userId);
+
+    //删除用户收货地址
+    public void deleteAddress(Long id);
+
+    //修改用户收货地址
+
+    public Address updateAddrss(Long id);
+
+    //编辑收货用户地址
+    public void editorAddress(Long id);
 }
--- pyg_service_user/src/main/java/com/itheima/pyg/service/user/AddressServiceImpl.java
+++ pyg_service_user/src/main/java/com/itheima/pyg/service/user/AddressServiceImpl.java
@@ -4,10 +4,10 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.itheima.pyg.dao.address.AddressDao;
 import com.itheima.pyg.pojo.address.Address;
 import com.itheima.pyg.pojo.address.AddressQuery;
-import com.itheima.pyg.service.user.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -27,5 +27,57 @@ public class AddressServiceImpl implements AddressService {
         criteria.andUserIdEqualTo(userId);
         List<Address> addressList = addressDao.selectByExample(query);
         return addressList;
+    }
+
+    //查询用户地址
+    @Override
+    public List<Address> findListByUserName(String userName) {
+        AddressQuery addressQuery = new AddressQuery();
+        addressQuery.createCriteria().andUserIdEqualTo(userName);
+        return addressDao.selectByExample(addressQuery);
+    }
+
+    ////新增用户收货地址
+    @Transactional
+    @Override
+    public void addAddress(Address address, String userId) {
+        Address address1 = new Address();
+        address1.setUserId(userId);
+
+        address1.setMobile(address.getMobile());
+        address1.setAddress(address.getAddress());
+        address1.setContact(address.getContact());
+        address1.setIsDefault("0");
+        address1.setNotes(address.getNotes());
+        address1.setCreateDate(new Date());
+        addressDao.insertSelective(address1);
+
+    }
+
+
+    //删除用户收货地址
+    @Transactional
+    @Override
+    public void deleteAddress(Long id) {
+        addressDao.deleteByPrimaryKey(id);
+    }
+
+    //修改用户收货地址
+    @Override
+    public Address updateAddrss(Long id) {
+        return addressDao.selectByPrimaryKey(id);
+    }
+
+
+    //编辑收货用户地址
+    @Override
+    public void editorAddress(Long id) {
+        AddressQuery query = new AddressQuery();
+        query.createCriteria().andIdEqualTo(id);
+        addressDao.deleteByExample(query);
+        Address address1 = new Address();
+
+        addressDao.insertSelective(address1);
+
     }
 }
